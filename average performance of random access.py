839c4288011aa68d73b7af549ea756d3daba8d1c
dberliner <danberliner@comcast.net>
2014-05-04 00:27:47 -0500

    Added transfer rate scripts

---
+++ average performance of random access.py
@@ -0,0 +1,38 @@
+print("All input fields are integers\n");
+print("NOTICE: IN THE SLIDES VALUES ARE SOMETIMES MB = KB/1000 \n")
+
+emulate = int(raw_input("0 to emulate this error, 1 to not: "))
+if emulate == 0:
+	kbtomb = 1024.0
+else: 
+	mbtomb=1000.0
+
+
+rpm = raw_input("RPM (leave blank if rev/sec is given): ")
+rps = raw_input("RPS (leave blank if rev/min is given): ")
+
+if rpm != "":
+	rps=int(rpm)/60
+else:
+	rps=int(rps)
+
+trans = int(raw_input("Enter sector size (in bytes)"))
+sectors = int(raw_input("Enter sectors per track "))
+
+
+seek = int(raw_input("Enter seek time in ms "))
+seek = float(seek)
+print("seek " + str(seek))
+
+rotLatency = 1.0/rps * 0.5 * 1000.0 # in ms
+print("rotLatency: " + str(rotLatency))
+SATR = float(rps * sectors * trans)/(1024.0*kbtomb) #in MB
+print("SATR: " + str(SATR))
+readTime = (float(trans/(1024.0*1000.0))/SATR)*kbtomb # in ms (lovely unit conversions)
+print("Total readtime: " + str(readTime))
+totalSeek = seek + rotLatency + readTime # in ms
+print("Total seek: " + str(totalSeek))
+ETR = float(1/totalSeek) * float(trans/1024.0)
+
+print("Effective transferring rate: " + str(ETR))
+raw_input("Press any key to exit");
---
+++ sustained average transfer rate.py
@@ -0,0 +1,25 @@
+print("All input fields are integers\n");
+print("NOTICE: IN THE SLIDES THE ANSWER IN MB WAS CALCULATED AS KB/1000 \n")
+
+emulate = int(raw_input("0 to emulate this error, 1 to not: "))
+if emulate == 0:
+	kbtomb = 1024.0
+else: 
+	mbtomb=1000.0
+
+rpm = raw_input("RPM (leave blank if rev/sec is given): ")
+rps = raw_input("RPS (leave blank if rev/min is given): ")
+
+if rpm != "":
+	rps=int(rpm)/60
+else:
+	rps=int(rps)
+
+trans = int(raw_input("Enter sector size (in bytes)"))
+sectors = int(raw_input("Enter sectors per track "))
+
+print("Sustained Average: " + str(rps * sectors * trans) + " bytes per second")
+print("Sustained Average: " + str(float(rps * sectors * trans)/1024) + " kb per second")
+print("Sustained Average: " + str(float(rps * sectors * trans)/(1024*kbtomb)) + " mb per second")
+print("Sustained Average: " + str(float(rps * sectors * trans)/(1024*kbtomb*1024)) + " gb per second")
+raw_input("Press any key to exit");
